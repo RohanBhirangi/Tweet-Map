f1b822253c2525a6c7edd82faecf6f16f714775a
RohanBhirangi <rohanbhirangi17@gmail.com>
2018-01-24 11:50:41 -0500

    Pushed code, venv and screenshots

--- TweetMap/properties.py
+++ TweetMap/properties.py
@@ -1,14 +1,14 @@
 def getTwitterCredentials():
-    consumer_key = "ng31hQ5CpE0CeocXAvhXorJnD"
-    consumer_secret = "SGdmJ0azAKQjDWVxeJrSxkPgyNOUx9FsV5bde0HU9kjOblKVjx"
-    access_token = "3238199665-dAeuSAYkzZidzjrfpVqM9WxA47XU1xFGGiDL859"
-    access_secret = "p8gxzHjCkuCL5TeCmVOj79ODObA1nB8pfTAYISLoDlXeL"
+    consumer_key = ""
+    consumer_secret = ""
+    access_token = ""
+    access_secret = ""
     return (consumer_key, consumer_secret, access_token, access_secret)
 
 
 def getElasticsearchCredentials():
-    host = "search-tweetmap-qoevmt735h4c3xbtq25gltymiy.us-east-1.es.amazonaws.com"
-    access_key = "AKIAIWHHD4EPO2FLJ5LA"
-    access_secret = "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"
-    region = "us-east-1"
+    host = ""
+    access_key = ""
+    access_secret = ""
+    region = ""
     return (host, access_key, access_secret, region)
